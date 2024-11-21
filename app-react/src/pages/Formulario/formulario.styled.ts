90027924a014b827424908fc99030ef507b238b3
Wagner <wagner.worst@gmail.com>
2024-11-21 19:16:19 -0300

    implementa validacao formulario

--- app-react/src/pages/Formulario/formulario.styled.ts
+++ app-react/src/pages/Formulario/formulario.styled.ts
@@ -27,9 +27,34 @@ padding-top: 5vh;
     font-size: 15px;
   }
 
+  .inputInvalido{
+    width: 70%;
+    height: 3vh;
+    border-radius: 15px;
+    background-color: rgba(0,0,0,.5%);
+    padding: 7px;
+    color: white;
+    font-family: "Roboto", serif;
+    font-size: 15px;
+    border-color: rgba(201,85,85,1);;
+  }
+
   textarea{
     border-radius: 15px;
     background-color: rgba(0,0,0,.5%);
+  }
+
+  span{
+    width: 96%;
+    text-align: center;
+    background-color: rgba(201,85,85,1);
+    border-radius: 15px;
+    color: rgba(0,0,0,1);
+    padding: 10px;
+  }
+
+  .hidden{
+    display: none;
   }
 }
 `;
--- app-react/src/pages/Formulario/index.tsx
+++ app-react/src/pages/Formulario/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import BotaoCustom from "../../Components/Botao/botao.styled"
 
 interface FormularioProps{
@@ -5,17 +6,70 @@ interface FormularioProps{
 }
 
 const Formulario = ({className}:FormularioProps) => {
+
+  const [nome, setNome] = useState("");
+  const [empresa, setEmpresa] = useState("");
+  const [mensagem, setMensagem ] = useState("")
+  const [exibirMsg, setExibirMsg ] = useState(false)
+
+  const atualizaDados = (event: React.ChangeEvent<HTMLInputElement>)=>{
+    if(event.target.name === "Nome"){
+      setNome (event.target.value);
+      return;
+    }
+
+    if(event.target.name === "Empresa"){
+      setEmpresa (event.target.value);
+      return;
+    }
+  };
+
+  const validaFormulario = () =>{
+    if(nome == "" || empresa == "")
+    {
+      setMensagem("Nome e Empresa são obrigatórios");
+      setExibirMsg(true)
+      return;
+    }
+    
+    if(nome && empresa)
+      {
+        setExibirMsg(false)
+        return;
+      }
+  }
+
   return (
     <>
       <div className={`${className}`}>
         <div className="formulario">
           <label htmlFor="Recrutador">Nome</label>
-          <input type="text" name="Nome" placeholder="Wagner Willian Worst"/>
+          <input 
+            className={`${exibirMsg ? "inputInvalido" : ""}`}
+            type="text" 
+            name="Nome" 
+            placeholder="Wagner Willian Worst"
+            onChange={atualizaDados}
+          />
+
           <label htmlFor="Empresa">Empresa</label>
-          <input type="text" name="Empresa" placeholder="Wagner's Corp"/>
+          <input 
+            className={`${exibirMsg ? "inputInvalido" : ""}`}
+            type="text" 
+            name="Empresa" 
+            placeholder="Wagner's Corp"
+            onChange={atualizaDados}
+          />
+          
           <label htmlFor="Empresa">Motivo Contato</label>
           <textarea name="postContent" rows={9} cols={40} />
-          <BotaoCustom texto="Enviar" severidade="sucesso"/>
+
+          <BotaoCustom 
+            texto="Enviar" 
+            severidade="sucesso"
+            onClick={validaFormulario}
+          />
+          <span className={`${exibirMsg ? "" : "hidden"}`}>{mensagem}</span>
         </div>
       </div>
     </>
